b3b110b37a8d90dcfc021f077ae17b303c481429
Der Portugiese <danilyapin17@gmail.com>
2025-10-17 12:51:14 +0200

    created register for user

---
+++ backend/src/main/java/com/dl17/backend/Service/UserService.java
@@ -0,0 +1,43 @@
+package com.dl17.backend.Service;
+
+import com.dl17.backend.DTO.UserDTO;
+import com.dl17.backend.Exception.ThisAccountNotFoundException;
+import com.dl17.backend.Exception.UserWithThisUsernameAlreadyExistException;
+import com.dl17.backend.Model.Account;
+import com.dl17.backend.Model.User;
+import com.dl17.backend.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(UserDTO userDTO) {
+        if (userRepository.findByUsername(userDTO.getUsername()).isEmpty()) {
+
+            String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+            User user = User.builder()
+                    .username(userDTO.getUsername())
+                    .email(userDTO.getEmail())
+                    .password(encodedPassword)
+                    .build();
+
+            return userRepository.save(user);
+        } else {
+            throw new UserWithThisUsernameAlreadyExistException("This username exists already.");
+        }
+    }
+}
