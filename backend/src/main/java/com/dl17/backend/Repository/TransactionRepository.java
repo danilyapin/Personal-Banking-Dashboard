e471fcdd26037542fb641357feb485cf3dc579ac
Der Portugiese <danilyapin17@gmail.com>
2025-10-17 09:36:42 +0200

    add repositories and mongodb connection

---
+++ backend/src/main/java/com/dl17/backend/Repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.dl17.backend.Repository;
+
+import com.dl17.backend.Model.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+}
