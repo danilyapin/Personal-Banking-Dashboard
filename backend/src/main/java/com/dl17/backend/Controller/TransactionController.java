3e929d4bdac31da7fe337074d9ff5786e1867aeb
Der Portugiese <danilyapin17@gmail.com>
2025-10-20 00:10:59 +0200

    added method into Repository class updated transaction model for mongodb, specially because of id updated Controller updated service

--- backend/src/main/java/com/dl17/backend/Controller/TransactionController.java
+++ backend/src/main/java/com/dl17/backend/Controller/TransactionController.java
@@ -1,18 +1,17 @@
 package com.dl17.backend.Controller;
 
 import com.dl17.backend.DTO.TransactionDTO;
-import com.dl17.backend.Exception.TransactionNotFoundException;
 import com.dl17.backend.Model.Transaction;
 import com.dl17.backend.Service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/transactions")
+@RequestMapping("/api/accounts/{accountId}/transactions")
 public class TransactionController {
 
     private final TransactionService transactionService;
@@ -22,30 +21,40 @@ public class TransactionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Transaction>> getTransactions() {
-        List<Transaction> transactions = transactionService.getTransactions();
+    public ResponseEntity<List<Transaction>> getTransactions(@PathVariable String accountId) {
+        String username = getLoggedInUsername();
+        List<Transaction> transactions = transactionService.getTransactionsByAccount(username, accountId);
         return ResponseEntity.ok(transactions);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Transaction>> getTransactionById(@PathVariable String id) {
-        Optional<Transaction> transaction = transactionService.getTransactionById(id);
-        return ResponseEntity.ok(transaction);
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable String accountId ,@PathVariable String id) {
+        String username = getLoggedInUsername();
+        return transactionService.getTransactionById(username, accountId, id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @PostMapping
-    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) {
-        Transaction transaction = transactionService.createTransaction(transactionDTO);
+    public ResponseEntity<Transaction> createTransaction(@PathVariable String accountId, @RequestBody TransactionDTO transactionDTO) {
+        String username = getLoggedInUsername();
+        Transaction transaction = transactionService.createTransaction(username, accountId, transactionDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteTransactionById(@PathVariable String id) {
-        try {
-            transactionService.deleteTransactionById(id);
-            return new ResponseEntity<>("Transaction deleted with success",HttpStatus.OK);
-        } catch (TransactionNotFoundException e) {
-            return new ResponseEntity<>("Transaction not found with id: " + id, HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> deleteTransactionById(@PathVariable String accountId, @PathVariable String id){
+        String username = getLoggedInUsername();
+        boolean deleted = transactionService.deleteTransactionById(username, accountId, id);
+        if (deleted) {
+            return ResponseEntity.ok("Transaction has been deleted successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Transaction with id " + id + " was not found.");
         }
+    }
+
+    private String getLoggedInUsername(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
--- backend/src/main/java/com/dl17/backend/Repository/TransactionRepository.java
+++ backend/src/main/java/com/dl17/backend/Repository/TransactionRepository.java
@@ -4,6 +4,9 @@ import com.dl17.backend.Model.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    List<Transaction> findByAccountId(String accountId);
 }
