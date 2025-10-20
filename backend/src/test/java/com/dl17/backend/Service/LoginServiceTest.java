ae68c9a551060e3c3bb17b9f44eaed3814dc2f87
Der Portugiese <danilyapin17@gmail.com>
2025-10-20 14:48:55 +0200

    created test for LoginService

---
+++ backend/src/test/java/com/dl17/backend/Service/LoginServiceTest.java
@@ -0,0 +1,44 @@
+package com.dl17.backend.Service;
+
+import com.dl17.backend.DTO.UserDTO;
+import com.dl17.backend.Model.User;
+import com.dl17.backend.Repository.UserRepository;
+import com.dl17.backend.Util.JwtUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class LoginServiceTest {
+
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+    private final JwtUtil jwtUtil = mock(JwtUtil.class);
+    private final LoginService loginService = new LoginService(userRepository, bCryptPasswordEncoder, jwtUtil);
+
+    @Test
+    void login() {
+        UserDTO userDTO = new UserDTO("username", "password");
+        User user = User.builder()
+                        .userId("abc123")
+                        .username(userDTO.getUsername())
+                        .password("encodedPassword")
+                        .build();
+
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
+        when(bCryptPasswordEncoder.matches(userDTO.getPassword(), user.getPassword())).thenReturn(true);
+        when(jwtUtil.generateToken(user.getUsername())).thenReturn("token");
+
+        String result = loginService.login(userDTO);
+
+        assertEquals("token", result);
+        assertEquals("username", user.getUsername());
+
+        verify(userRepository, times(1)).findByUsername(user.getUsername());
+        verify(bCryptPasswordEncoder, times(1)).matches(userDTO.getPassword(), user.getPassword());
+        verify(jwtUtil, times(1)).generateToken(user.getUsername());
+    }
+}
